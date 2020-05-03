92cc911c2ed51bfd99d7ed64bcc7e9920b5e1879
SMukund1 <SMukund1@TM0495633.gsm1900.org>
2020-05-03 02:26:53 -0700

    Added comments to delete contact

---
+++ Contacts_API/src/main/java/net/codejava/ListContacts.java
@@ -0,0 +1,24 @@
+package net.codejava;
+import java.util.ArrayList;
+
+public class ListContacts {
+	ArrayList<Contact> list;
+	
+	public ListContacts() {
+		list = new ArrayList<Contact>();
+	}
+
+	public ArrayList<Contact> getList() {
+		return list;
+	}
+
+	public void setList(Contact c) {
+		this.list.add(c);
+	}
+	
+	//delete contact from list
+	public void delete(Contact c) {
+		this.list.remove(c);
+	}
+	
+}
